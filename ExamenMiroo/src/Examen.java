318fa77ab164fef55890230bf4c3c10968ada3bb
theGeo <theGeo@theGeo-PC>
2018-02-27 10:05:28 +0100

    2 cambi

--- ExamenMiroo/src/Examen.java
+++ ExamenMiroo/src/Examen.java
@@ -3,7 +3,6 @@ public class Examen {
 	public static void main(String[]args) {
 		printOne();
 		printOne();
-		printTwo();
 	}
 	public static void printOne() {
 		System.out.println("hello world");
